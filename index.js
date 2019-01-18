f21081b78d142241033cb7f8ce35201b24659146
Mehdi M. Saei <mahdi.saeikhalili@gmail.com>
2019-01-18 14:22:25 -0500

    realtime database using firebase snapshot

--- index.js
+++ index.js
@@ -74,11 +74,30 @@ const update = (data) => {
 
 }
 
-db.collection('dishes').get().then(res => {
-    var data = [];
-    res.docs.forEach(doc => {
-        data.push(doc.data())
-    })
+var data = [];
+
+db.collection('dishes').onSnapshot(res => {
+
+    res.docChanges().forEach(change => {
+
+        const doc = {...change.doc.data(), id: change.doc.id };
+
+        switch (change.type) {
+            case 'added':
+                data.push(doc);
+                break;
+            case 'modified':
+                const index = data.findIndex(item => item.id == doc.id);
+                data[index] = doc;
+                break;
+            case 'removed':
+                data = data.filter(item => item.id !== doc.id);
+                break;
+            default:
+                break;
+        }
+
+    });
 
     update(data);
 
